62f41d6e9002cb434b8d2a85751e239bb37c010f
Lisa Sun <lisasun@Lisas-MacBook-Pro.local>
2020-07-01 14:57:07 -0500

    Add in antoehr function test

---
+++ amplify/backend/function/amplifyTest4/src/index.js
@@ -0,0 +1,14 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	FUNCTION_REACTAMPLIFYFUNCTION3_NAME
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
